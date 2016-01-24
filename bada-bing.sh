13d95d8334a7409d158b9c2d86aec233578dbed0
Mykal <mcnichol.m@gmail.com>
2016-01-23 18:11:33 -0600

    Python Script Error handling, Bash syntax highlighting

--- bada-bing.sh
+++ bada-bing.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
+msg(){
+    if [ -n "$2" ]; then
+	echo $2"Bash:\t$1 $NO_COLOUR"
+    else 
+	echo $WHITE"Bash:\t$1 $NO_COLOUR"
+    fi
+}
+
+newsect(){
+    echo ""
+    msg "$1" $GREEN
+}
+
+BLUE="\x1b[0;34m"
+RED="\x1b[0;31m"
+GREEN="\x1b[0;32m"
+YELLOW="\x1b[1;33m"
+WHITE="\x1b[1;37m"
+LIGHT_RED="\x1b[1;31m"
+LIGHT_GREEN="\x1b[1;32m"
+LIGHT_BLUE="\x1b[1;34m"
+LIGHT_CYAN="\x1b[1;36m"
+NO_COLOUR="\x1b[0m"
+
+msg "Got the ball.  Let's get Rolling"
 
 # Code taken from https://gist.github.com/mokagio/b974620ee8dcf5c0671f
-echo "Checking Xcode CLI tools"
+newsect "Checking Xcode CLI tools"
 # Only run if the tools are not installed yet
 # To check that try to print the SDK path
 xcode-select -p &> /dev/null
 if [ $? -ne 0 ]; then
-  echo "Xcode CLI tools not found. Installing them..."
+  msg "Xcode CLI tools not found. Installing them..."
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
   PROD=$(softwareupdate -l |
     grep "\*.*Command Line" |
@@ -16,16 +41,16 @@ if [ $? -ne 0 ]; then
     tr -d '\n')
   softwareupdate -i "$PROD" -v;
 else
-  echo "Xcode CLI tools OK"
+  msg "Xcode CLI tools OK"
 fi
 
-echo "Installing Homebrew - The Missing Package Manager for Mac"
+newsect "Installing Homebrew - The Missing Package Manager for Mac"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "Installing Commandline Toolset"
+newsect "Installing Commandline Toolset"
 brew install wget
 
-echo "Installing Application Toolset"
+newsect "Installing Application Toolset"
 brew cask install iterm2
 brew cask install google-chrome 
 brew cask install skype 
@@ -34,3 +59,7 @@ brew cask install handbrake
 brew cask install spotify 
 brew cask install dropbox 
 brew cask install the-unarchiver
+
+msg "Looks like my job is done here....back to you Python"
+
+
--- install.py
+++ install.py
@@ -4,8 +4,23 @@
 import subprocess
 
 def main():
-    subprocess.call('./bada-bing.sh',shell=True)
-    return 1
+    print "Python:\tHandoff to Bash"
+    execScript()
+    print "Python:\tLooks like I'm done here...Python Out"
+    return 
+
+def execScript():
+    try: 
+        subprocess.call('./bada-bing.sh',shell=True)
+    except OSError as e:
+	print "What we've got here is a failure to communicate. ~ This Script"
+	print "Sadly there was an error.  Hopefully you are smart enough to fix it"
+	print "Probably something with the shell script, 'bada-bing.sh', here's a hint: "
+	print e.errno
+	print e.filename
+	print e.strerror
+	return
+     
 
 if __name__ == '__main__':
     main()
